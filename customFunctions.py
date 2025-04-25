fa2cf0e2ab3d7f45292d7e77a1850a29af42ada7
JLSM02 <jsalasmontoro@gmail.com>
2025-04-25 12:15:00 +0200

    updated CustomFunctions

--- customFunctions.py
+++ customFunctions.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from qiskit.circuit import QuantumCircuit, Parameter
 from qiskit.quantum_info import SparsePauliOp
 from qiskit.primitives import Estimator
+from scipy.optimize import minimize
 
 
 # ====================================================================
@@ -53,7 +54,9 @@ def build_deep_ansatz(num_qubits):
         layer(qc, theta_layer)
         qc.barrier()
 
-    return qc, thetas
+    # Devuelve el cirvuito y el numero de parametros
+    num_params =  len(thetas)*num_qubits
+    return qc, num_params
 
 
 
@@ -94,12 +97,7 @@ def evaluate_deriv(params, ansatz, observable, index, estimator):
 # ====================================================================
 #            Función para la obtener las varianzas
 # ====================================================================
-def get_variances_data(base_observable, n_qubits, index, num_shots=1000):
-    estimator = Estimator()
-
-    current_observable=expand_observable(base_observable, n_qubits)
-    ansatz_circuit, thetas = build_deep_ansatz(n_qubits)
-
+def get_variances_data(observable, ansatz, num_params, index, num_shots=1000):
     # Lista para guardar los valores esperados
     value_list = []
 
@@ -108,17 +106,129 @@ def get_variances_data(base_observable, n_qubits, index, num_shots=1000):
 
     for _ in range(num_shots):
 
-        rand_param_vector = 2 * np.pi *np.random.random(len(thetas)*n_qubits)
+        rand_param_vector = 2 * np.pi *np.random.random(num_params)
 
-        value = evaluate_observable(rand_param_vector, ansatz_circuit, current_observable, estimator)
-        deriv = evaluate_deriv(rand_param_vector, ansatz_circuit, current_observable, index, estimator)
+        value = evaluate_observable(rand_param_vector, ansatz, observable, estimator)
+        deriv = evaluate_deriv(rand_param_vector, ansatz, observable, index, estimator)
 
         value_list.append(value)
         value_list.append(deriv)
 
-    return [np.var(value_list), np.var(deriv_list), n_qubits]
+    return np.var(value_list), np.var(deriv_list)
 
 
+
+# ====================================================================
+#            Función para minimización VQE
+# ====================================================================
+def VQE_minimization(ansantz_function, minQubits, maxQubits, base_observable, index):
+
+    print_logo()
+
+    for i in range(minQubits, maxQubits+1):
+
+        estimator = Estimator()
+        
+        current_observable=expand_observable(base_observable, i)
+        ansatz_circuit, num_params = ansantz_function(i)
+
+        # Parámetros iniciales
+        initial_param_vector = np.zeros(num_params)
+
+        # Información sobre la iteración actual
+        print("\n=====================================================")
+        print(f"Preparando ejecución para {i} qubits.")
+        print(f"Se usarán {num_params} parámetros")
+
+        # Diccionario para almacenar la evolución del costo
+        cost_history_dict = {
+            "iters": 0,
+            "cost_history": [],
+            "deriv_history": [],
+        }
+
+        # If index = None, the derivative is not calculated
+        if index==None:
+            def cost_func(params, ansatz, observable, index, estimator):
+
+                cost = evaluate_observable(params, ansatz, observable, estimator)
+
+                cost_history_dict["iters"] += 1
+                cost_history_dict["cost_history"].append(cost)
+
+                return cost
+        else:
+            def cost_func(params, ansatz, observable, index, estimator):
+
+                cost = evaluate_observable(params, ansatz, observable, estimator)
+                deriv = evaluate_deriv(params, ansatz, observable, index, estimator)
+
+                cost_history_dict["iters"] += 1
+                cost_history_dict["cost_history"].append(cost)
+                cost_history_dict["deriv_history"].append(deriv)
+
+                return cost
+
+        # Ejecutamos la optimización
+        res = minimize(
+            cost_func,
+            initial_param_vector,
+            args=(ansatz_circuit, current_observable, index, estimator),
+            method="COBYLA",
+        )
+
+        # Graficar evolución del costo
+        fig, ax = plt.subplots()
+        ax.plot(range(cost_history_dict["iters"]), cost_history_dict["cost_history"], label="Funcion de costo")
+        ax.plot(range(cost_history_dict["iters"]), cost_history_dict["deriv_history"], label="Derivada")
+        ax.set_xlabel("Iteraciones")
+        ax.set_ylabel("Energía")
+        ax.set_title(f"Minimización para {i} qubits")
+        plt.legend()
+        plt.show()
+
+        print(f"Fin ejecución con {i} qubits. Mínimo encontrado: {res.fun}")
+        print("=====================================================")
+
+
+# ====================================================================
+#            Función para varianza de gradientes
+# ====================================================================
+def variance_vs_nQubits(ansantz_function, minQubits, maxQubits, base_observable, index, shots):
+    data = []
+
+    print_logo()
+
+    for i in range(minQubits, maxQubits+1):
+
+        estimator = Estimator()
+        
+        current_observable=expand_observable(base_observable, i)
+        ansatz_circuit, num_params = ansantz_function(i)
+
+        # Información sobre la iteración actual
+        print("\n=====================================================")
+        print(f"Calculando varianzas con {i} qubits.\n")
+        
+        result = get_variances_data(current_observable, ansatz_circuit, num_params, index, shots)
+        print(f"Varianza del valor esperado: {result[0]}")
+        print(f"Varianza de la derivada: {result[1]}")
+
+        data.append(result)
+
+    data = np.array(data)
+    # Grafica concentracion del resultado y su derivada
+    fig, ax = plt.subplots()
+    ax.scatter(data[:,2], data[:,1], label="Var(E)")
+    ax.scatter(data[:,2], data[:,0], label=r"Var($\partial$E)")
+    ax.set_xlabel("N qubits")
+    ax.set_ylabel("var(E)")
+    ax.set_title(f"BP en VQE")
+    ax.set_yscale("log")
+    ax.legend()
+    plt.show()
+
+    return data
 
 
 
